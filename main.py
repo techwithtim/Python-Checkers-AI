3e51642f5b000b21aa5f05b78e8e05e8a77c974b
Sami <samiabdulsater@gmail.com>
2022-03-18 19:10:54 +0100

    MC tree structure begin

--- main.py
+++ main.py
@@ -32,8 +32,8 @@ def main():
 
 
         elif game.turn == RED:
-            value, new_board = minimax(game.get_board(), 3, False, game)
-            #new_board = montecarlots(game.get_board(), RED)
+            #value, new_board = minimax(game.get_board(), 3, False, game)
+            new_board = montecarlots(game.get_board(), RED)
             if new_board == None :
                 run = False
 
@@ -55,7 +55,7 @@ def main():
 #                game.select(row, col)
 #                game.get_board().get_num()
 
-        pygame.time.delay(3000)
+        pygame.time.delay(5000)
         game.update()
     print("And the winner is : ", game.winner())
     pygame.quit()
--- montecarlo/algorithm.py
+++ montecarlo/algorithm.py
@@ -1,27 +1,61 @@
+from collections import defaultdict
 from copy import deepcopy
 
-def montecarlots(board, player) :
-    moves = get_all_moves(board, player)
-    if len(moves) != 0 :
-        return moves[0]
 
-    return None
+def montecarlots(board, player):
+    tree = MonteCarloTreeSearchNode(board, player)
+    moves = tree._untried_actions
+
+    return moves[0]
 
 
-def get_all_moves(board, color):
-    moves = []
-    for piece in board.get_all_pieces(color):
-        valid_moves = board.get_valid_moves(piece)
-        if len(valid_moves) !=0 :
-            #print("Piece ", piece, " of player ", color, " can do the following moves ")
-            #print("\tIn short : ")
-            for move, skip in valid_moves.items():
-                #print("\t", move, ", resulting in ", len(skip), " pieces of the other color captured")
-                temp_board = deepcopy(board)
-                temp_piece = temp_board.get_piece(piece.row, piece.col)
-                new_board = simulate_move(temp_piece, move, temp_board, skip)
-                moves.append(new_board)
-    return moves
+
+
+class MonteCarloTreeSearchNode():
+    def __init__(self, state, color, parent=None, parent_action=None):
+        """
+        Class that modelizes a node as manipulated in a MCTS
+        :param state:   Current board
+        :param color:   Current player color
+        :param parent:  Parent node, if any (none by default, for root)
+        :param parent_action:   pas encore compris ce paramètre
+        """
+        self.state = state
+        self.color = color
+        self.parent = parent
+        self.parent_action = parent_action
+        self.children = []
+        self._number_of_visits = 0
+        self._results = defaultdict(int)
+        self._results[1] = 0
+        self._results[-1] = 0
+        self._untried_actions = None
+        self._untried_actions = self.untried_actions()
+        return
+
+    def get_all_moves(self):
+        """
+            Function that returns all the possible outcoming boards from the current position when it is the turn of player "color"
+            :param board: Current board
+            :param color: Current player
+            :return: List of boards corresponding to possible outcomes
+        """
+        moves = []
+        for piece in self.state.get_all_pieces(self.color):
+            valid_moves = self.state.get_valid_moves(piece)
+            if len(valid_moves) != 0:
+                for move, skip in valid_moves.items():
+                    print(move)
+                    temp_board = deepcopy(self.state)
+                    temp_piece = temp_board.get_piece(piece.row, piece.col)
+                    new_board = simulate_move(temp_piece, move, temp_board, skip)
+                    moves.append(new_board)
+        return moves
+
+
+    def untried_actions(self):
+        self._untried_actions = self.get_all_moves()
+        return self._untried_actions
 
 
 def simulate_move(piece, move, board, skip):
