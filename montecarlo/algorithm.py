628e6e3764b04028cd4c5587522a929c0cbef781
Sami <samiabdulsater@gmail.com>
2022-03-19 00:21:28 +0100

    Arborescence MC : expansion function (with TODO)

--- montecarlo/algorithm.py
+++ montecarlo/algorithm.py
@@ -2,6 +2,10 @@ from collections import defaultdict
 from copy import deepcopy
 
 
+RED = (255,0,0)
+WHITE = (255, 255, 255)
+
+
 def montecarlots(board, player):
     tree = MonteCarloTreeSearchNode(board, player)
     moves = tree._untried_actions
@@ -15,6 +19,7 @@ class MonteCarloTreeSearchNode():
     def __init__(self, state, color, parent=None, parent_action=None):
         """
         Class that modelizes a node as manipulated in a MCTS
+        :param game:    Current game
         :param state:   Current board
         :param color:   Current player color
         :param parent:  Parent node, if any (none by default, for root)
@@ -57,6 +62,15 @@ class MonteCarloTreeSearchNode():
         self._untried_actions = self.get_all_moves()
         return self._untried_actions
 
+    def expand(self):
+        next_state = self._untried_actions.pop()
+        next_color = RED if self.color == WHITE else WHITE
+        # TODO : faire en sorte que les actions soient différents des états.
+        #   Action = mouvement de la piece (x1, y1) à la case (x2, y2).
+        #   Etat : plateau après le mouvement
+        child_node = MonteCarloTreeSearchNode(next_state, color=next_color, parent_action=next_state)
+        self.children.append(child_node)
+        return child_node
 
 def simulate_move(piece, move, board, skip):
     board.move(piece, move[0], move[1])
