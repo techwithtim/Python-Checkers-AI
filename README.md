e5fdc595c59f20f3f675cb9712380412b2828f69
Diego Rubas <diegrubas@gmail.com>
2022-03-25 04:09:54 +0100

    Added poetry launch options (minimax/mcts)

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# Python-Checkers-AI
-A checkers AI using the minimax algorithm.
+# Python-Checkers-AI (Checkers)
+
+You will find hereunder the instructions and details of the checkers game.
+
+The game is presented here with 2 AI techniques, search with the **Minimax Algorithm** and search with the **Monte Carlo Tree Search (MCTS) Algorithm**.
+
+## Installation
+
+In order to install the game, copy the repository with the git tool:
+
+```
+git clone https://github.com/jemappellesami/Python-Checkers-AI
+```
+
+Then, access the game:
+
+```bash
+cd Python-Checkers-AI
+```
+
+After having installed Python and Poetry, install the project dependancies:
+
+```bash
+poetry install
+```
+
+## Utilisation
+
+You can then launch the game in the newly-created virtual environment.
+
+When launching the game, you will need to choose which algorithms you will face or which will face each other, for instance:
+
+```bash
+poetry run python main.py --p1 minimax --p2 mcts
+```
+
+starts a game Minimax AI vs. MCTS AI.
+
+It is possible to play against an AI by adapting the command as follows:
+
+```bash
+poetry run python main.py --p1 human --p2 mcts
+```
+
+You will then need to select which piece to move and where to place it during your turn.
+
+The `--p1` et `--p2` options can take as argument minimax, mcts or human.
+
+In summary:
+```
+usage: main.py [-h] --player1 {minimax,mcts,human} --player2 {minimax,mcts,human}
+
+Checkers Game
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --player1 {minimax,mcts,human}, --p1 {minimax,mcts,human}, -1 {minimax,mcts,human}
+                        Type of player for player 1
+  --player2 {minimax,mcts,human}, --p2 {minimax,mcts,human}, -2 {minimax,mcts,human}
+                        Type of player for player 2
+
+```
+
+![checkers screen](../assets/img/checkers.png)
--- main.py
+++ main.py
@@ -4,13 +4,15 @@ from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE, RED, WHITE
 from checkers.game import Game
 from minimax.algorithm import minimax
 from montecarlo.algorithm import montecarlots
+import argparse
 
 FPS = 60
 
-RED = (255,0,0)
+RED = (255, 0, 0)
 WHITE = (255, 255, 255)
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Checkers')
+
 
 def get_row_col_from_mouse(pos):
     x, y = pos
@@ -18,51 +20,104 @@ def get_row_col_from_mouse(pos):
     col = x // SQUARE_SIZE
     return row, col
 
+
 def main():
+    """
+    One can run games of checkers between different types of bot
+    and algorithms by giving as argument the variables above.
+    """
     run = True
     clock = pygame.time.Clock()
     game = Game(WIN)
 
+    parser = argparse.ArgumentParser(description="The Connect 4 game")
+    parser.add_argument(
+        "--player1",
+        "--p1",
+        "-1",
+        type=str,
+        help="Type of player for player 1",
+        required=True,
+        choices=("minimax", "mcts"),
+        # choices=("minimax", "mcts", "random", "human"),
+        default="minimax",
+    )
+    parser.add_argument(
+        "--player2",
+        "--p2",
+        "-2",
+        type=str,
+        help="Type of player for player 2",
+        required=True,
+        choices=("minimax", "mcts"),
+        # choices=("minimax", "mcts", "random"),
+        default="mcts",
+    )
+    args = parser.parse_args()
+
+    # p = [None, None]
+    # if args.player1 == "minimax":
+    #     p[0] = MINIMAX
+    # else:
+    # # elif args.player1 == "mcts":
+    #     p[0] = MONTE_CARLO
+    #
+    # if args.player2 == "minimax":
+    #     p[1] = MINIMAX
+    # else:
+    # # elif args.player2 == "mcts":
+    #     p[1] = MONTE_CARLO
+
+    p = [args.player1, args.player2]
+    # p_str = [args.player1, args.player2]
+
     while run:
         clock.tick(FPS)
-        
-        if game.turn == WHITE :
-            print("Minimax AI is thinking")
-            value, new_board = minimax(game.get_board(), 3, True, game)
-            game.ai_move(new_board)
-            print("Minimax AI has made its move")
 
+        if game.turn == WHITE:
+            print("Player 1 ({} AI) is thinking".format(p[0].upper()))
+            if p[0] == "minimax":
+                value, new_board = minimax(game.get_board(), 3, True, game)
+                game.ai_move(new_board)
+            elif p[0] == "mcts":
+                new_board = montecarlots(game.get_board(), RED)
+                if new_board is None:
+                    run = False
+                else:
+                    game.ai_move(new_board)
+            print("Player 1 ({} AI) has made its move".format(p[0].upper()))
 
         elif game.turn == RED:
-            #value, new_board = minimax(game.get_board(), 3, False, game)
-            print("Monte Carlo AI is thinking")
-            new_board = montecarlots(game.get_board(), RED)
-            if new_board == None :
-                run = False
-
-            else :
+            print("Player 2 ({} AI) is thinking".format(p[1].upper()))
+            if p[1] == "minimax":
+                value, new_board = minimax(game.get_board(), 3, True, game)
                 game.ai_move(new_board)
-                print("Monte Carlo AI has made its move")
+            elif p[1] == "mcts":
+                new_board = montecarlots(game.get_board(), RED)
+                if new_board is None:
+                    run = False
+                else:
+                    game.ai_move(new_board)
+            print("Player 2 ({} AI) has made its move".format(p[1].upper()))
 
-
-        if game.winner() != None:
+        if game.winner() is not None:
             run = False
 
+        #        for event in pygame.event.get():
+        #            if event.type == pygame.QUIT:
+        #                run = False
+        #
+        #            if event.type == pygame.MOUSEBUTTONDOWN:
+        #                move = montecarlots(game.get_board(), WHITE)
+        #                pos = pygame.mouse.get_pos()
+        #                row, col = get_row_col_from_mouse(pos)
+        #                game.select(row, col)
+        #                game.get_board().get_num()
 
-#        for event in pygame.event.get():
-#            if event.type == pygame.QUIT:
-#                run = False
-#
-#            if event.type == pygame.MOUSEBUTTONDOWN:
-#                move = montecarlots(game.get_board(), WHITE)
-#                pos = pygame.mouse.get_pos()
-#                row, col = get_row_col_from_mouse(pos)
-#                game.select(row, col)
-#                game.get_board().get_num()
-
-        #pygame.time.delay(5000)
+        # pygame.time.delay(5000)
         game.update()
     print("And the winner is : ", game.winner())
     pygame.quit()
+
 
 main()
