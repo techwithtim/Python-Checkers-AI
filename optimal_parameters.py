0ce23ea99e23c999d1d852727cbb54c5e2ef24a7
Alexandre Flachs <alexandre.flachs@ulb.be>
2022-04-19 16:31:06 +0200

    Setup for genetic algorithm

--- optimal_parameters.py
+++ optimal_parameters.py
@@ -1,79 +1,84 @@
 # Let's gooooo
+from typing import List
+
 from checkers.board import Board
-from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE, RED, WHITE
+from checkers.constants import RED, WHITE
 import random
-import pygame
 
 from checkers.game import Game
-from main import make_move
+from main import make_ai_move
 from montecarlo.algorithm import MCNode
 
 FPS = 60
 random.seed(15)
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('Checkers')
 POP_SIZE = 3
+NB_GAMES = 2
 
-def init_population():
+
+class Villager:
+    def __init__(self, it, safe_heuri, exploit, reward=None):
+        self.it = it
+        self.safe_heuri = safe_heuri
+        self.exploit_param = exploit
+        self.reward = reward
+
+
+def init_population() -> List[Villager]:
     population = []
     for i in range(POP_SIZE):
         it = random.randint(5, 40)
         safe_heuri = random.uniform(0, 1)
         exploit = random.uniform(0, 1)
-        population.append((it, safe_heuri, exploit))
+        population.append(Villager(it, safe_heuri, exploit))
     return population
 
 
-def main():
-    clock = pygame.time.Clock()
+def make_move(game, p, run, tree):
+    """
+    Executes a move on the board determined by the arguments chosen at game launch.
+    """
+    n = -1
+    if game.turn == WHITE:
+        n = 0
+    elif game.turn == RED:
+        n = 1
+    else:
+        print("Error : game.turn is neither WHITE nor RED")
+
+    run, tree = make_ai_move(game, n, p, run, tree)
+    return run, tree
 
 
-
-    p = ["minimax", "mcts"]
-
-    optimal = False
-    population = init_population()
-    while not optimal:
-        for villager in population:
-            set_parameters(villager[0], villager[1], villager[2])
-            reward = 0
-            for i in range(3):
-                reward += play_game(clock, p)
-            print(reward)
-    pygame.quit()
-
-
-def play_game(clock, p):
-    game = Game(WIN)
+def play_game(p) -> float:
+    """
+    Play a game and return the reward of the game seen from the mcts view
+    :param p: players setup
+    :return: float reward value seen from mcts
+    """
+    game = Game()
     winner = 0
     most_recent_tree = None
     running = True
 
     while running:
-        clock.tick(FPS)
+        running, most_recent_tree = make_move(game, p, running, most_recent_tree)
 
-        if game.turn == WHITE:
-            running, most_recent_tree = make_move(game, p, 0, running, most_recent_tree)
-            if not running:
-                print("Blanc a gagné")
+        if not running:
+            if game.turn == WHITE:
                 winner = "RED"
-        elif game.turn == RED:
-            running, most_recent_tree = make_move(game, p, 1, running, most_recent_tree)
-            if not running:
-                print("Rouge a gagné")
-                print(running)
+            elif game.turn == RED:
                 winner = "WHITE"
 
         if game.winner() is not None:
             running = False
             winner = game.winner()
-        game.update()
-        # pygame.time.wait(100)
-        print(running)
 
+    print("Partie de simulation terminée")
     if winner == "RED":
+        print("Rouge a gagné")
         return 1
     elif winner == "WHITE":
+        print("Blanc a gagné")
         return 0
     else:
         return 0.5
@@ -83,6 +88,26 @@ def set_parameters(iterations, safe_heuristic, exploitation):
     Board.set_safe_heuri_param(safe_heuristic)
     MCNode.set_exploit(exploitation)
     MCNode.set_max_it(iterations)
+
+
+def main():
+    p = ["minimax", "mcts"]
+
+    optimal = False
+    population = init_population()
+    j = 0
+    while not optimal:
+        for villager in population:
+            set_parameters(villager.it, villager.safe_heuri, villager.exploit_param)
+            reward = 0
+            for i in range(NB_GAMES):
+                j += 1
+                reward += play_game(p)
+                print(j, "'th game is over")
+            villager.reward = reward
+        population.sort(key=lambda x: x.reward, reverse=True)
+        # TODO: select x% of best villagers and fuse them
+        break
 
 
 if __name__ == '__main__':
